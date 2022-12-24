d75ec94858e38a3fb9acc2c0bdcd2f73da2055ec
Joabe Soares Teixeira de Souza <joabe.soares1@gmail.com>
2022-12-24 15:35:34 -0300

    go11

---
+++ funcoes/recursividade/recursividade.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fatorial(n int) (int, error) {
+	switch  {
+	case n < 0:
+		return -1, fmt.Errorf("Número inválido: %d", n)
+	case n == 0:
+		return 1, nil
+	default:
+		fatorialanterior, _ := fatorial(n - 1)
+		return n * fatorialanterior, nil 
+	}
+}
+
+func main() {
+	resultado, _ := fatorial(5)
+	fmt.Println(resultado)
+}
